1f1ba5a26beb4bf492e425b3c729c7304c01433a
unreliableMerge <aloyna2@yandex.ru>
2016-06-05 20:57:59 +0300

    some missed dependencies

--- app/data-core/md5HashAlgorithm.js
+++ app/data-core/md5HashAlgorithm.js
@@ -192,3 +192,25 @@ var toMd5 = function (str) {
 
     return temp.toLowerCase();
 }
+
+function utf8_encode(str_data) {
+
+    str_data = str_data.replace(/\r\n/g, "\n");
+    var utftext = "";
+
+    for (var n = 0; n < str_data.length; n++) {
+        var c = str_data.charCodeAt(n);
+        if (c < 128) {
+            utftext += String.fromCharCode(c);
+        } else if ((c > 127) && (c < 2048)) {
+            utftext += String.fromCharCode((c >> 6) | 192);
+            utftext += String.fromCharCode((c & 63) | 128);
+        } else {
+            utftext += String.fromCharCode((c >> 12) | 224);
+            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+    }
+
+    return utftext;
+}
