01cf1ffa7ee164277257974c52958e57ab7a1277
unreliableMerge <aloyna2@yandex.ru>
2016-06-15 01:22:10 +0300

    preparings for animations investigate

--- app/app.module.js
+++ app/app.module.js
@@ -6,6 +6,7 @@ angular.
            'initialState',
            'searchResults',
            'resultDetails',
-           'favouritesList'
+           'favouritesList',
+           'ngAnimate'
         ]);
 
